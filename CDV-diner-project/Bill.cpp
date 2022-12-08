a20ffcf45d9dc1cdd9130761254fe493ba8fc314
agk-code <agorniak-kurzawa@edu.cdv.pl>
2022-12-08 23:02:22 +0100

    bill generating

--- CDV-diner-project/Bill.cpp
+++ CDV-diner-project/Bill.cpp
@@ -1,13 +1,13 @@
 #include "Bill.h"
-
+#include "CartProduct.h"
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-Bill::Bill(){}
+Bill::Bill() {}
 
-Bill::~Bill(){}
+Bill::~Bill() {}
 
 void Bill::createBill(Cart cart)
 {
@@ -16,4 +16,20 @@ void Bill::createBill(Cart cart)
 	cout << cart.readCartList().size() << endl;
 	system("pause");
 
+	fstream plik;
+	plik.open("bill.txt", ios::out | ios::app);
+	
+	for (int i = 0; i < cart.readCartList().size(); i++) {
+		plik << (i + 1) << ". "
+			<< cart.readCartList()[i].readProductName()
+			<< "   "
+			<< cart.readCartList()[i].readProductPrice()
+			<< "   "
+			<< cart.readCartList()[i].readProductTime()
+			<< "   "
+			<< cart.readCartList()[i].readProductQuantity()
+			<< endl;
+	}
+
+	plik.close();
 }
