048945f90a596f106bf1c4cad94112f83dbfddad
Arian Orwat <arian.orwat@gmail.com>
2022-12-08 02:38:50 +0100

    Menu fixes, table choose and restaurant data

--- CDV-diner-project/CDV-diner-project.cpp
+++ CDV-diner-project/CDV-diner-project.cpp
@@ -14,6 +14,7 @@ using namespace std;
 // Global varibles
 string name = "";
 bool delivery = false;
+int tableNumber = 1;
 
 // Test function for create products list
 void createProductsList(ProductsList *productsList) {
@@ -33,6 +34,6 @@ int main()
 
 	// Initializing basic objects
 	createProductsList(&productsList);
-	menu.initMenu(&name, productsList);
+	menu.initMenu(&name, productsList, &tableNumber);
 
 }
--- CDV-diner-project/Menu.h
+++ CDV-diner-project/Menu.h
@@ -12,13 +12,15 @@ private:
 	char chooseDish;
 	char chooseBasket;
 	Cart cart;
+	void showRestaurantData();
 	void setName(std::string* name);
+	void setTable(int* tableNumber);
 	void categoryMenu(ProductsList productList);
 	void dishesMenu(Category category);
 	void basketMenu();
 public:
 	Menu(Cart *_cart);
 	~Menu();
-	void initMenu(std::string *name, ProductsList productList);
+	void initMenu(std::string *name, ProductsList productList, int *tableNumber);
 };
 
